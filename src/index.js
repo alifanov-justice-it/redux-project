f81816f16be6a809a4cd01b16eaf9724646ebe51
Alifanov Valentin <va.alifanov@it-justice.com>
2022-03-02 17:12:34 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {compose, createStore, applyMiddleware} from "redux";
+import {rootReducer} from "./redux/rootReducer";
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {forbiddenWordsMiddleware} from "./redux/middleware";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(
+        thunk, forbiddenWordsMiddleware
+    ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(app,
   document.getElementById('root')
 );
 
